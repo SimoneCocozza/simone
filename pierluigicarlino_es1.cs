13b074b0c930fe9db1e9d0e7bd4a5c63b0a5905c
GNP50 <pierluigicarlino50@gmail.com>
2021-10-20 12:22:39 +0200

    Update pierluigicarlino_es1.cs

--- pierluigicarlino_es1.cs
+++ pierluigicarlino_es1.cs
@@ -8,8 +8,8 @@ public class Program
 	{
 		List<string> dates = new List<string>();
    
-    	for(int i=0;i<20;i++)
-        	dates.Add(i+"/1/2021");
+    		for(int i=0;i<20;i++)
+        		dates.Add(i+"/1/2021");
 
 		for(int i=0;i<dates.Count;i++)Console.Write(dates[i] + (i==dates.Count-1 ? "" : "-"));
 	}
