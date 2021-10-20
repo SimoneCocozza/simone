1f236c982a67cddac0212123deb4a9c2c50c6d77
GNP50 <pierluigicarlino50@gmail.com>
2021-10-20 12:22:08 +0200

    Create pierluigicarlino_es1.cs

---
+++ pierluigicarlino_es1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	
+	public static void Main()
+	{
+		List<string> dates = new List<string>();
+   
+    	for(int i=0;i<20;i++)
+        	dates.Add(i+"/1/2021");
+
+		for(int i=0;i<dates.Count;i++)Console.Write(dates[i] + (i==dates.Count-1 ? "" : "-"));
+	}
+} 
