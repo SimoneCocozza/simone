8c98765dbb27988f0f825449db4299d1a070ab77
Matteo <56003378+mMontinaro@users.noreply.github.com>
2021-10-20 14:08:32 +0200

    Dates with String Builder

---
+++ matteomontinaro_es1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * @author: Matteo Montinaro
+ */
+namespace ConsoleApp1
+{
+    class matteomontinaro_es1
+    {
+        static void Main(string[] args)
+        {
+            List<String> myDates = new List<string>() { "11/10/2021", "12/10/2021", "13/10/2021", "14/10/2021", "15/10/2021" };
+        
+            StringBuilder sDates = new StringBuilder();
+
+            foreach(String date in myDates)
+            {
+                sDates.Append(date + "\n");
+            }
+
+            Console.WriteLine("\nAll Dates in List:\n" + sDates);
+        }
+    }
+}
