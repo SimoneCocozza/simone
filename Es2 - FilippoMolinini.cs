bc1f6baad7eb5809ebc39b9f6e314f9286b60a55
Filippo Molinini <92856639+Filippo-Molinini@users.noreply.github.com>
2021-10-20 17:12:30 +0200

    Add files via upload

---
+++ Es2 - FilippoMolinini.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> miedate = new List<string>();
+           
+            miedate.Add("01/01/2011");
+            miedate.Add("01/02/1980");
+            miedate.Add("01/03/1995");
+
+            foreach (string d in miedate)
+            {
+                var mese = d.Split('/');
+                var conversione = Convert.ToInt32(mese[1]);
+
+                //Console.WriteLine(mese[1]);
+                //Console.WriteLine(conversione);
+
+                try
+                {
+                    if (conversione % 2 == 1)
+                    {
+
+                        throw new Exception("Mese " + conversione + " dispari\n");
+
+                    }
+
+                }
+
+                catch (Exception ecc)
+                {
+
+                    File.AppendAllText("MesiD.txt", ecc.Message);
+                
+                }
+
+            }
+
+        }
+
+    }
+
+}
