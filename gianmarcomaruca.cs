52381d230faa7fead9032014ff1ae8755eb48be3
Gianmarco Maruca <gianmarco.maruca@finconsgroup.local>
2021-11-10 14:22:51 +0100

    Esercizi 1-3

---
+++ gianmarcomaruca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> date = new List<string>() { "12/11/1948", "28/09/1991", "15/12/2007", "11/11/2011" };
+
+            StringBuilder dateConcatenate = new StringBuilder();
+
+            // Concatena tutte le date
+            foreach (string data in date)
+            {
+                dateConcatenate.Append(data + " - ");
+            }
+
+            Console.WriteLine(dateConcatenate);
+        }
+    }
+}
---
+++ gianmarcomaruca_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> date = new List<string>() { "12/03/1948", "28/09/1991", "15/12/2007", "11/11/2011" };
+
+            // Stampa i mesi dispari su un file di testo
+            foreach (string data in date)
+            {
+                try
+                {
+                    string[] splittedData = data.Split("/");
+                    var mese = Convert.ToInt32(splittedData[1]);
+
+                    if (mese % 2 != 0)
+                        throw new ArithmeticException("Il mese " + mese + " è dispari.");
+                }
+                catch (Exception mese)
+                {
+                    using (StreamWriter writer = new StreamWriter("C:\\Users\\hp\\Documents\\log.txt", true))
+                    {
+                        writer.WriteLine("Eccezione: " + mese);
+                    }
+                }
+            }
+        }
+    }
+}
