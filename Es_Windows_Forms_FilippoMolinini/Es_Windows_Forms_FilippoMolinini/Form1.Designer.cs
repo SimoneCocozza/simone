37bdfd817c2206834d88ffb8f126ef924c5cded8
Filippo Molinini <filippo.molinini@finconsgroup.local>
2021-11-10 14:22:47 +0100

    Aggiunta cartella esercizio Windows Forms

---
+++ Es_Windows_Forms_FilippoMolinini/Es_Windows_Forms_FilippoMolinini/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Es_Windows_Forms_FilippoMolinini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1017, 473);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(24, 505);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(427, 101);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Crea file di nomi";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(616, 505);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(427, 101);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Elabora file";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1067, 623);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Esercizio WF - Filippo Molinini";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Es_Windows_Forms_FilippoMolinini/Es_Windows_Forms_FilippoMolinini/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es_Windows_Forms_FilippoMolinini
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Creates a text file containing 100 random generated names.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("listanomi.txt", "");                 //Creates a new empty text file.
+                    
+            for (int i = 0; i < 100; i++)
+            {
+                File.AppendAllText("listanomi.txt", Path.GetRandomFileName() + "\r\n");     //Creates a random name and writes it inside the previously created text file.
+            }
+
+            textBox1.Text = File.ReadAllText("listanomi.txt");      //Shows the previously written text file's contents on the screen.
+
+        }
+
+
+        //Creates a new text file based on the previous one showing the amount of vowels inside every name
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] nomigen;                                       //Creates a new list of string
+
+            nomigen = File.ReadAllLines("listanomi.txt");           //Writes every name from the previously created text file inside every string item in the list
+
+            string[] vocali = { "a", "e", "i", "o", "u" };          //Creates a new list containing every vowel
+
+            int conta = 0;
+
+            File.WriteAllText("listanomivocali.txt", "");           //Creates a new empty text file.
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (File.ReadAllLines("listanomi.txt")[i].Contains(vocali[j]))         //Checks if a vowel is present inside every name. 
+                    {
+
+                        conta++;                                    //Increases the counter by one.
+
+                    }
+                
+                }
+
+                File.AppendAllText("listanomivocali.txt", File.ReadAllLines("listanomi.txt")[i] + " = " + conta + "\r\n");      //Takes every line from the previously generated file, adds new info and writes them inside a new text file.
+
+                textBox1.Text = File.ReadAllText("listanomivocali.txt");        //Shows the newly written text file's contents on the screen.
+
+                conta = 0;                                          //Resets the counter to 0.
+
+            }
+
+        }
+    
+    }
+}
